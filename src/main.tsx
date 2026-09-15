a54c2ce689fa2ee53a5490261e5046d4b820f738
Armelle Cogent <armelle.cogent@ign.fr>
2026-09-15 18:32:28 +0200

    @fontsource-variable/open-sans pour encore alléger

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 
 
-import '@fontsource/open-sans/latin-400.css';
-import '@fontsource/open-sans/latin-ext-400.css';
-import '@fontsource/open-sans/latin-500.css';
-import '@fontsource/open-sans/latin-ext-500.css';
-import '@fontsource/open-sans/latin-600.css';
-import '@fontsource/open-sans/latin-ext-600.css';
-import '@fontsource/open-sans/latin-700.css';
-import '@fontsource/open-sans/latin-ext-700.css';
+import './styles/fonts.css';
 import '@fontsource/fira-sans/latin-400.css';
 import '@fontsource/fira-sans/latin-ext-400.css';
 import '@fontsource/fira-sans/latin-500.css';
